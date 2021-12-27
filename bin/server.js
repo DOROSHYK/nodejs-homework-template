e9a2a3b1b41a17a457e3cc38fdd2b4e43673fde7
Doroshuk <dorosukolga100@gmail.com>
2021-12-27 22:52:42 +0200

    add schema and model

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,22 @@
-const app = require('../app')
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000
+const app = require('../app');
 
-app.listen(PORT, () => {
+// const DB_HOST = 'mongodb+srv://olala:0970187156@cluster0.0qy5z.mongodb.net/phoneBook?retryWrites=true&w=majority'
+
+const {DB_HOST,PORT = 3000} = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
 })
+    console.log("database connect success")
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
+
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { NotFound, BadRequest } = require('http-errors');
 const router = express.Router();
 const Joi = require('joi');
-const contactsOperations = require("../../model/index");
-
+// const contactsOperations = require("../../model/index");
+const { Contact } = require("../../model/contacts");
 
 const joiSchema = Joi.object({
   name: Joi.string().required(),
@@ -11,37 +11,37 @@ const joiSchema = Joi.object({
   phone: Joi.string().required(),
 })
 
-router.get('/', async (_, res, next) => {
+// router.get('/', async (_, res, next) => {
 
-  try {
-    const contacts = await contactsOperations.listContacts();
-  res.json(contacts);
-   }
-  catch (error) {
-    next(error);
+//   try {
+//     const contacts = await contactsOperations.listContacts();
+//   res.json(contacts);
+//    }
+//   catch (error) {
+//     next(error);
     
-  }
+//   }
   
-})
+// })
 
-router.get('/:contactId', async (req, res, next) => {
- const { contactId } = req.params;
-  try {
+// router.get('/:contactId', async (req, res, next) => {
+//  const { contactId } = req.params;
+//   try {
    
-    const result = await contactsOperations.getContactById(contactId);
+//     const result = await contactsOperations.getContactById(contactId);
     
-    if (!result) {
-      throw new NotFound('Contact not found'); 
+//     if (!result) {
+//       throw new NotFound('Contact not found'); 
     
      
-    }
-    res.json(result);
-  } 
-  catch (error) {
-    next(error);
+//     }
+//     res.json(result);
+//   } 
+//   catch (error) {
+//     next(error);
   
-   }
-})
+//    }
+// })
 
 
 router.post('/', async (req, res, next) => {
@@ -51,7 +51,7 @@ router.post('/', async (req, res, next) => {
       throw new BadRequest(error.message);
      
     }
-    const result = await contactsOperations.addContact(req.body)
+    const result = await Contact.create(req.body)
     res.status(201).json(result);
    } catch (error) {
     next(error); 
@@ -59,40 +59,40 @@ router.post('/', async (req, res, next) => {
 })
 
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsOperations.removeContact(contactId);
-    if (!result) {
-      throw new NotFound(`Contact with id=${contactId} not found`);
-    }
-    res.json({"message": "contact deleted"})
+// router.delete('/:contactId', async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+//     const result = await contactsOperations.removeContact(contactId);
+//     if (!result) {
+//       throw new NotFound(`Contact with id=${contactId} not found`);
+//     }
+//     res.json({"message": "contact deleted"})
     
-  } catch (error) {
-    next(error);
-  }
-})
+//   } catch (error) {
+//     next(error);
+//   }
+// })
 
 
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
+// router.put('/:contactId', async (req, res, next) => {
+//   try {
     
-     const { error } = joiSchema.validate(req.body);
-    if (error) {
-      throw new BadRequest(error.message);
+//      const { error } = joiSchema.validate(req.body);
+//     if (error) {
+//       throw new BadRequest(error.message);
      
-    }
-    const { contactId } = req.params;
-    const result = await contactsOperations.updateContact({ contactId, ...req.body} );
-    if (!result) {
-      throw new NotFound(`Contact with id=${contactId} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-   }
+//     }
+//     const { contactId } = req.params;
+//     const result = await contactsOperations.updateContact({ contactId, ...req.body} );
+//     if (!result) {
+//       throw new NotFound(`Contact with id=${contactId} not found`);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//    }
   
-})
+// })
 
 module.exports = router
