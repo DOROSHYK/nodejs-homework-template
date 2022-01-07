a1d5805cd5f0ae70c4428007c3d750c8d50aef20
Doroshuk <dorosukolga100@gmail.com>
2022-01-08 00:30:39 +0200

    Create an endpoint /singup

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,33 @@
 const express = require('express');
-const Joi = require("joi");
+const {  BadRequest, Conflict } = require('http-errors')
 
+const { User } = require('../../model/user');
+const { joiSchema } = require('../../model/user');
 const router = express.Router();
 
-router.post('/signup',)
+router.post('/signup', async (req, res, next) => {
+    try {
+        const { error } = joiSchema.validate(req.body);
+        if (error) {
+           throw new BadRequest(error.message); 
+        }
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            throw new Conflict("Email in use");
+        }
+        const newUser = await User.create(req.body);
+        res.status(201).json({
+             user: {
+    email: newUser.email,
+    subscription: "starter"
+  }
+        })
+    }
+    catch (error) {
+      next(error)  
+    }
+}
+)
 
 module.exports = router
