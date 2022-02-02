0b771674572fa647171c90e01035e69be208fda6
Doroshuk <dorosukolga100@gmail.com>
2022-02-02 18:32:30 +0200

    Fix

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,14 +2,14 @@
 const sgMail = require('@sendgrid/mail'); 
 require('dotenv').config();
 
-const { SENGRID_API_KEY } = process.env;
+const { SENGRID_API_KEY, EMAIL_KEY } = process.env;
 sgMail.setApiKey(SENGRID_API_KEY);
 
 
 
 const sendEmail = async (data) => {
     try {
-        const email = { ...data, from: 'dorosukolga100@gmail.com' };
+        const email = { ...data, from: EMAIL_KEY };
         await sgMail.send(email);
         return true;
     } catch (error) {
